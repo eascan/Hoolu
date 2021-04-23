faf749929db1205a2b4b4b055e4269eea33c7bcc
Edward Ascan <edward.ascan@me.com>
2021-04-23 12:18:23 -0400

    add API key, create results component, add async query function to home

---
+++ components/Results.js
@@ -0,0 +1,5 @@
+function Results() {
+  return <div className=""></div>;
+}
+
+export default Results;
--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import Head from "next/head";
 import Header from "../components/Header";
 import Nav from "../components/Nav";
+import Results from "../components/Results";
 
 export default function Home() {
   return (
@@ -14,7 +15,11 @@ export default function Home() {
 
       <Nav />
 
-      {/* Results */}
+      <Results />
     </div>
   );
+}
+
+export async function getServerSideProps(context) {
+  const genre = context.query.genre;
 }
