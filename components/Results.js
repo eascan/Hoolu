c5cafc8fe7d2d7d368d2df73c9ecf48c1ef7f6fc
Edward Ascan <edward.ascan@me.com>
2021-04-27 23:54:08 -0400

    add result and thumbnail component, integrate with moviedb database

--- components/Results.js
+++ components/Results.js
@@ -1,5 +1,13 @@
-function Results() {
-  return <div className=""></div>;
+import Thumbnail from "./Thumbnail";
+
+function Results({results}) {
+  return (
+    <div className="">
+      {results.map((result) => (
+        <Thumbnail key={result.id} result={result} />
+      ))}
+    </div>
+  );
 }
 
 export default Results;
