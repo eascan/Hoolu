e6e08975dd1e20374ff9d3b4c4a548b11420ed9f
Edward Ascan <edward.ascan@me.com>
2021-04-28 16:25:23 -0400

    add styling to results

--- components/Thumbnail.js
+++ components/Thumbnail.js
@@ -4,7 +4,6 @@ import {ThumbUpIcon} from "@heroicons/react/outline";
 function Thumbnail({result}) {
   const BASE_URL = "https://image.tmdb.org/t/p/original/";
 
-  console.log(result);
   return (
     <div className="p-2 group cursor-pointer transition duration-200 ease-in transform sm:hover:scale-105 hover:z-50">
       <Image
